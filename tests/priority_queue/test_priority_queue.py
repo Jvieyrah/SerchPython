f540d8fe5e383cf856a1be124086bed99ac696d0
jvieirah <me.contrate.aero@gmail.com>
2023-04-09 19:37:36 -0300

    teste do teste

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,3 +1,44 @@
+# from ting_file_management.priority_queue import PriorityQueue
+# import pytest
+
+
+# def test_basic_priority_queueing():
+#     """Aqui irá sua implementação"""
+#     queue = PriorityQueue()
+
+#     with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+#         queue.search(12)
+
+#         small_enought_to_be_a_priority = {
+#             'nome_do_arquivo': 'test.txt',
+#             'qtd_linhas': 4,
+#             'linhas_do_arquivo': ['1', '2', '3', '4'],
+#         }
+
+#         too_big_to_be_a_priority = {
+#             'nome_do_arquivo': 'test.txt',
+#             'qtd_linhas': 6,
+#             'linhas_do_arquivo': ['1', '2', '3', '4', '5', '6'],
+#         }
+
+#         queue.enqueue(too_big_to_be_a_priority)
+#         queue.enqueue(small_enought_to_be_a_priority)
+
+#         assert len(queue) == 2
+
+#         item0 = queue.search(0)
+#         assert item0['qtd_linhas'] == 4
+
+#         item1 = queue.search(1)
+#         assert item1['qtd_linhas'] == 6
+
+#         queue.dequeue()
+#         assert len(queue) == 1
+
+#         item1 = queue.search(0)
+#         assert item1['qtd_linhas'] == 6
+
+# from ting_file_management.priority_queue import PriorityQueue
 from ting_file_management.priority_queue import PriorityQueue
 import pytest
 
@@ -7,33 +48,40 @@ def test_basic_priority_queueing():
     queue = PriorityQueue()
 
     with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
-        queue.search(12)
+        queue.search(50)
 
-        small_enought_to_be_a_priority = {
-            'nome_do_arquivo': 'test.txt',
-            'qtd_linhas': 4,
-            'linhas_do_arquivo': ['1', '2', '3', '4'],
-        }
+    priority = {
+        "nome_do_arquivo": "teste.txt",
+        "qtd_linhas": 3,
+        "linhas_do_arquivo": ["linha1", "linha2", "linha3"],
+    }
 
-        too_big_to_be_a_priority = {
-            'nome_do_arquivo': 'test.txt',
-            'qtd_linhas': 6,
-            'linhas_do_arquivo': ['1', '2', '3', '4', '5', '6'],
-        }
+    not_priority = {
+        "nome_do_arquivo": "teste.txt",
+        "qtd_linhas": 6,
+        "linhas_do_arquivo": [
+            "linha1",
+            "linha2",
+            "linha3",
+            "linha4",
+            "linha5",
+            "linha6",
+        ],
+    }
 
-        queue.enqueue(too_big_to_be_a_priority)
-        queue.enqueue(small_enought_to_be_a_priority)
+    queue.enqueue(not_priority)
+    queue.enqueue(priority)
 
-        assert len(queue) == 2
+    assert len(queue) == 2
 
-        item0 = queue.search(0)
-        assert item0['qtd_linhas'] == 4
+    item = queue.search(0)
+    assert item["qtd_linhas"] == 3
 
-        item1 = queue.search(1)
-        assert item1['qtd_linhas'] == 6
+    item2 = queue.search(1)
+    assert item2["qtd_linhas"] == 6
 
-        queue.dequeue()
-        assert len(queue) == 1
+    queue.dequeue()
+    assert len(queue) == 1
 
-        item1 = queue.search(0)
-        assert item1['qtd_linhas'] == 6
+    item3 = queue.search(0)
+    assert item3["qtd_linhas"] == 6
